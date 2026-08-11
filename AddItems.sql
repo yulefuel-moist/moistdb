a35c7d56ec260f7f304b78bd7cf7759b04a439ba
Yulefuel <yulefuels@gmail.com>
2026-08-11 23:43:02 +0200

    Updated BWL+MC and AQ40 points 11/8 2026

---
+++ AddItems.sql
@@ -0,0 +1,4 @@
+INSERT OR IGNORE INTO Items (item_id, raid_id, item_name, date_added)
+VALUES
+    (19352, 2, 'Chromatically Tempered Sword', '2026-08-06'),
+    (21650, 3, 'Ancient Qiraji Ripper', '2026-08-06');
